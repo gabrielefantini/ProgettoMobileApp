f7d406c8c930998d0424a85e96e3b79a1d4b06fb
gabrielefantini <gabrielefantini97@gmail.com>
2021-05-07 11:24:20 +0200

    refactoring of LoginFragment, still not working

--- app/src/main/java/it/polito/mad/group25/lab/fragments/login/LoginFragment.kt
+++ app/src/main/java/it/polito/mad/group25/lab/fragments/login/LoginFragment.kt
@@ -1,34 +1,105 @@
 package it.polito.mad.group25.lab.fragments.login
 
+
 import android.app.Activity
-import android.content.ContentValues.TAG
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.identity.GetSignInIntentRequest
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
-import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import it.polito.mad.group25.lab.R
 
-
 class LoginFragment: Fragment() {
-    private val RC_SIGN_IN = 7
+
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private val TAG = "LoginFragment"
+    private lateinit var mAuth: FirebaseAuth
+    private val REQUEST_CODE_GOOGLE_SIGN_IN = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container:  ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.login_fragment, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val signInButton = view.findViewById<SignInButton>(R.id.sign_in_button)
+        mAuth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(activity as Activity, gso)
+
+        signInButton.setOnClickListener {
+            signIn()
+        }
+    }
+
+    private val getGoogleDataBack =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult(),
+            ActivityResultCallback {
+                if (it.resultCode == REQUEST_CODE_GOOGLE_SIGN_IN) {
+                    Log.d(TAG, "------------------ACTIVITY START CALLBACK--------------------: ")
+                    val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+                    var userToken: String? = null
+
+                    try {
+                        var account = task.getResult(ApiException::class.java)
+                        if (account != null) {
+                            userToken = account.idToken
+                            Log.d(TAG, "----------------------------$userToken: ")
+                            FirebaseGoogleAuth(account)
+                        } else Log.d(TAG, "ACCOUNT IS NULL: ")
+                    } catch (e: Exception) {
+                        Log.d(TAG, ":FAILED TO GET USER ${e.message}")
+                    }
+
+                    //viewModel.handleEvent(LoginEvent.OnGoogleSignInResult(LoginResult(123, userToken!!)))
+                }
+                else{
+                    Log.d(TAG, "------------------RESULT ISN'T OKAY ${it.resultCode}  ----------------: ")
+                }
+            })
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        getGoogleDataBack.launch(signInIntent)
+    }
+    private fun FirebaseGoogleAuth(account: GoogleSignInAccount) {
+        val authCredential = GoogleAuthProvider.getCredential(account.idToken, null)
+        mAuth.signInWithCredential(authCredential)
+            .addOnCompleteListener( activity as Activity, OnCompleteListener<AuthResult>() {
+                fun onComplete(task: Task<AuthResult>){
+                    if(task.isSuccessful){
+                        Log.d(TAG, "Successfull Firebase Auth !!!")
+                    } else {
+                        Log.d(TAG, "UnSuccessfull Firebase Auth !!!")
+                    }
+                }
+            })
+    }
+    /*private val RC_SIGN_IN = 7
     private lateinit var mGoogleSignInClient: GoogleSignInClient
     private lateinit var resultLauncer: ActivityResultLauncher<Intent>
     private lateinit var auth: FirebaseAuth
@@ -77,8 +148,8 @@ class LoginFragment: Fragment() {
         }
     }
 
-    /*=======================================================
-    =======================================================*/
+    *//*=======================================================
+    =======================================================*//*
     private fun signIn(){
         val signInIntent = mGoogleSignInClient.signInIntent
         resultLauncer.launch(signInIntent)
@@ -111,5 +182,5 @@ class LoginFragment: Fragment() {
                     //utente non loggato, fai quello che devi
                 }
             }
-    }
+    }*/
 }
