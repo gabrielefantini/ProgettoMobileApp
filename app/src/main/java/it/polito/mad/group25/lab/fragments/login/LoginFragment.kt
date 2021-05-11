170cd6f8e36ad293dbe4f794323172abc985d297
Lorenzo <lorenzoappendini@gmail.com>
2021-05-11 16:51:21 +0200

    added "remember me" logic and authContext view model

--- app/src/main/java/it/polito/mad/group25/lab/fragments/login/LoginFragment.kt
+++ app/src/main/java/it/polito/mad/group25/lab/fragments/login/LoginFragment.kt
@@ -1,16 +1,19 @@
 package it.polito.mad.group25.lab.fragments.login
 
 import android.app.Activity
+import android.app.Application
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.MutableLiveData
 import androidx.navigation.findNavController
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -20,18 +23,21 @@ import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import it.polito.mad.group25.lab.R
-import it.polito.mad.group25.lab.fragments.trip.list.TripListViewModel
+import it.polito.mad.group25.lab.utils.persistence.Persistors
+import it.polito.mad.group25.lab.utils.viewmodel.PersistableViewModel
 
 class LoginFragment: Fragment(R.layout.login_fragment) {
 
     private lateinit var mGoogleSignInClient : GoogleSignInClient
     private lateinit var resultLauncher : ActivityResultLauncher<Intent>
-    private lateinit var auth: FirebaseAuth
-    private val tripListViewModel: TripListViewModel by activityViewModels()
+    private lateinit var authenticator: FirebaseAuth
+
+    private val authContext: AuthContext by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,7 +51,7 @@ class LoginFragment: Fragment(R.layout.login_fragment) {
 
         mGoogleSignInClient = GoogleSignIn.getClient(requireContext(),gso)
 
-        auth = Firebase.auth
+        authenticator = Firebase.auth
 
         resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ res ->
             if(res.resultCode == Activity.RESULT_OK) {
@@ -58,26 +64,38 @@ class LoginFragment: Fragment(R.layout.login_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<SignInButton>(R.id.signInButton)
-            .setOnClickListener { signIn() }
+        view.findViewById<SignInButton>(R.id.signInButton).also {
+            it.setSize(SignInButton.SIZE_STANDARD)
+            it.setOnClickListener { signIn() }
+        }
+
+        view.findViewById<CheckBox>(R.id.rememberMeCheck).setOnClickListener {
+            authContext.rememberMe.value = !authContext.rememberMe.value!!
+        }
+
     }
 
     override fun onStart() {
         super.onStart()
-        //check for already signed in users
-        val currentUser = auth.currentUser
-        currentUser?.let {
-            tripListViewModel.userId = currentUser.uid
-            //tripListViewModel.userId = "A"
-            //tripListViewModel.userId = "B"
-            activity?.findNavController(R.id.nav_host_fragment_content_main)
-                ?.navigate(R.id.action_LoginFragment_to_TripListFragment)
-        }
+
+        //check for already signed in users (only if selected)
+        if(authContext.rememberMe.value == true)
+            authenticator.currentUser?.let {
+                authContext.authUser = it
+                activity?.findNavController(R.id.nav_host_fragment_content_main)
+                    ?.navigate(R.id.action_LoginFragment_to_TripListFragment)
+            }
     }
 
+    //sign in to google
     private fun signIn(){
         var intent = mGoogleSignInClient.signInIntent
         resultLauncher.launch(intent)
+    }
+
+    //sign out from google
+    private fun signOut(){
+        mGoogleSignInClient.signOut()
     }
 
     private fun handleSignInResult(task: Task<GoogleSignInAccount>) {
@@ -93,15 +111,29 @@ class LoginFragment: Fragment(R.layout.login_fragment) {
 
     private fun firebaseAuthWithGoogle(idToken: String){
         val credentials= GoogleAuthProvider.getCredential(idToken,null)
-        auth.signInWithCredential(credentials)
+        authenticator.signInWithCredential(credentials)
             .addOnCompleteListener { task ->
                 if(task.isSuccessful){
                     //success
-                    val user = auth.currentUser
-                    tripListViewModel.userId = user.uid
+                    authContext.authUser = authenticator.currentUser!!
+
+                    //if "remember me" is not selected, next time login page is visited google shouldn ask again the google account
+                    if(authContext.rememberMe.value == false)
+                        signOut()
+
                     activity?.findNavController(R.id.nav_host_fragment_content_main)
                         ?.navigate(R.id.action_LoginFragment_to_TripListFragment)
                 }
             }
     }
+}
+
+class AuthContext(application: Application): PersistableViewModel(application){
+    var authUser: FirebaseUser? = null
+
+    var rememberMe: MutableLiveData<Boolean> by Persistors.sharedPreferences(
+        default = MutableLiveData(false)
+    )
+
+    fun userId(): String? = authUser?.uid
 }
