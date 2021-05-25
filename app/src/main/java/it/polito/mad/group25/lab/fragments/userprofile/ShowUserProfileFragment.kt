4ec64f2bf23fae105543fb46c4c5b5420feaa39c
Schiopu Andrei <a.schiopu@reply.it>
2021-05-25 20:17:38 +0200

    show user profile style

--- app/src/main/java/it/polito/mad/group25/lab/fragments/userprofile/ShowUserProfileFragment.kt
+++ app/src/main/java/it/polito/mad/group25/lab/fragments/userprofile/ShowUserProfileFragment.kt
@@ -5,20 +5,29 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.widget.Button
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import it.polito.mad.group25.lab.R
 
 class ShowUserProfileFragment :
     GenericUserProfileFragment(R.layout.show_user_profile_fragment) {
 
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         visualizeUserData(view, true)
+        view.findViewById<Button>(R.id.bought_trip_list_shortcut_button).setOnClickListener {
+            findNavController()
+                .navigate(R.id.action_showUserProfileFragment_to_BoughtTripsListFragment)
+        }
+        view.findViewById<Button>(R.id.trip_list_shortcut_button).setOnClickListener {
+            findNavController()
+                .navigate(R.id.action_showUserProfileFragment_to_TripListFragment)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        userProfileViewModel.shownUser.observe(this){
+        userProfileViewModel.shownUser.observe(this) {
             if (it.id == authenticationContext.userId())
                 inflater.inflate(R.menu.menu, menu)
         }
