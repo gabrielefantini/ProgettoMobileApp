4f76e91955d5ab51e96aefacc37988ba33636b20
Lorenzo <lorenzoappendini@gmail.com>
2021-05-31 20:18:24 +0200

    added no-locations case logic

--- app/src/main/java/it/polito/mad/group25/lab/fragments/map/MapFragment.kt
+++ app/src/main/java/it/polito/mad/group25/lab/fragments/map/MapFragment.kt
@@ -1,10 +1,15 @@
 package it.polito.mad.group25.lab.fragments.map
 
+import android.content.Context
 import android.graphics.Color
 import android.graphics.Paint
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import it.polito.mad.group25.lab.R
@@ -15,24 +20,47 @@ import org.osmdroid.views.overlay.Marker
 import org.osmdroid.views.overlay.Overlay
 import org.osmdroid.views.overlay.Polyline
 import org.osmdroid.config.Configuration.*
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class MapFragment: Fragment(R.layout.map_fragment) {
     private val mapViewModel: MapViewModel by activityViewModels()
 
     private lateinit var map : MapView
+    private lateinit var myLocationOverlay: MyLocationNewOverlay
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        map = MapView(inflater.context)
+        map.setDestroyMode(false)
+
+        return map
+    }
+
+    class MyGpsLocationProvider(context: Context?, private val viewModel: MapViewModel): GpsMyLocationProvider(context){
+        override fun onLocationChanged(location: Location) {
+            super.onLocationChanged(location)
+            if(viewModel.currentLocation != location)
+                viewModel.currentLocation = location
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val context = this.context
 
-        getInstance().load(context,androidx.preference.PreferenceManager.getDefaultSharedPreferences(context))
-
-        map = view.findViewById(R.id.map)
-        map.setTileSource(TileSourceFactory.MAPNIK)
-
         val mapController = map.controller
 
         val geopoints = mapViewModel.geopoints
+
+        // my location ***
+        myLocationOverlay = MyLocationNewOverlay(MyGpsLocationProvider(context,mapViewModel),map)
+        myLocationOverlay.enableMyLocation()
+        map.overlays.add(myLocationOverlay)
+        // ******
 
         var cLat = 0.0  //center latitude
         var cLong = 0.0 //center longitude
@@ -42,7 +70,11 @@ class MapFragment: Fragment(R.layout.map_fragment) {
             val marker = Marker(map)
             marker.position = gp
             marker.title = "loc $id"
-            marker.snippet = "description"
+            marker.snippet = when(id){
+                0 -> "Departure"
+                geopoints.lastIndex -> "Destination"
+                else -> "Intermediate stop"
+            }
             map.overlays.add(marker)
 
             cLat += gp.latitude
@@ -63,18 +95,39 @@ class MapFragment: Fragment(R.layout.map_fragment) {
         // last settings ***
         cLat /= geopoints.size
         cLong /= geopoints.size
-        mapController.setZoom(12.0)
-        mapController.setCenter(GeoPoint(cLat,cLong))
+
+        map.isTilesScaledToDpi = true
+        mapController.setZoom(10.0)
+
+        if(geopoints.size == 0){
+            //no locations -> set center to current position (if available, else use default)
+            val lastCurrentLocation = mapViewModel.currentLocation
+            if(lastCurrentLocation != null){
+                mapController.setCenter(GeoPoint(lastCurrentLocation.latitude,lastCurrentLocation.longitude))
+            }else{
+                mapController.setCenter(GeoPoint(45.0,7.0))
+            }
+        }else{
+            mapController.setCenter(GeoPoint(cLat,cLong))
+        }
         // *****************
     }
 
     override fun onResume() {
         super.onResume()
+        map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
         map.onResume()
+        myLocationOverlay.enableMyLocation()
     }
 
     override fun onPause() {
-        super.onPause()
         map.onPause()
+        myLocationOverlay.disableMyLocation()
+        super.onPause()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        map.onDetach()
     }
 }
--- app/src/main/java/it/polito/mad/group25/lab/fragments/map/MapViewModel.kt
+++ app/src/main/java/it/polito/mad/group25/lab/fragments/map/MapViewModel.kt
@@ -1,6 +1,7 @@
 package it.polito.mad.group25.lab.fragments.map
 
 import android.app.Application
+import android.location.Location
 import androidx.lifecycle.AndroidViewModel
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.overlay.Polyline
@@ -8,6 +9,8 @@ import kotlin.math.round
 
 class MapViewModel(application: Application): AndroidViewModel(application) {
     lateinit var geopoints: MutableList<GeoPoint>
+
+    var currentLocation: Location? = null
 
     fun distance(path: Polyline): Double = (path.distance/1000).round(2)
 }
