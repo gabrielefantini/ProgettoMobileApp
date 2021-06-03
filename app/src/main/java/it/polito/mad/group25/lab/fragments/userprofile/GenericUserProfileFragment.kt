2a543aee652c9ec3abcf4c7c4e938240e2ba6969
Schiopu Andrei <a.schiopu@reply.it>
2021-06-03 15:32:45 +0200

    edit trip fix

--- app/src/main/java/it/polito/mad/group25/lab/fragments/userprofile/GenericUserProfileFragment.kt
+++ app/src/main/java/it/polito/mad/group25/lab/fragments/userprofile/GenericUserProfileFragment.kt
@@ -5,9 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.RatingBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -41,7 +39,6 @@ abstract class GenericUserProfileFragment(
 
     fun visualizeUserData(view: View, hideSensitiveDataIfNecessary: Boolean) {
         userProfileViewModel.shownUser.observe(viewLifecycleOwner) { it ->
-            val userId = it.id
             if (it == null)
                 return@observe
 
@@ -52,27 +49,6 @@ abstract class GenericUserProfileFragment(
             view.findViewById<TextView>(R.id.fullName).text = it.fullName
             view.findViewById<TextView>(R.id.email).text = it.email
 
-            val driver_rate = reviewViewModel.reviews.values.toList()
-                .filter { reviewVM -> reviewVM.reviewed == userId && reviewVM.isReviewedDriver == true }
-                .map { reviewVM -> reviewVM.stars }
-            if (driver_rate.isNotEmpty()) {
-                var avg = 0F
-                driver_rate.forEach { avg += it!! }
-                avg = avg / driver_rate.size
-                view.findViewById<RatingBar>(R.id.driver_rate_bar).rating = avg
-            } else
-                view.findViewById<RatingBar>(R.id.driver_rate_bar).rating = 0F
-
-            val passenger_rate = reviewViewModel.reviews.values.toList()
-                .filter { reviewVM -> reviewVM.reviewed == userId && reviewVM.isReviewedDriver == false }
-                .map { reviewVM -> reviewVM.stars }
-            if (passenger_rate.isNotEmpty()) {
-                var avg = 0F
-                passenger_rate.forEach { avg += it!! }
-                avg = avg / passenger_rate.size
-                view.findViewById<RatingBar>(R.id.passenger_rate_bar).rating = avg
-            } else
-                view.findViewById<RatingBar>(R.id.passenger_rate_bar).rating = 0F
 
             val nickname = view.findViewById<TextView>(R.id.nickName)
             val location = view.findViewById<TextView>(R.id.location)
@@ -82,12 +58,7 @@ abstract class GenericUserProfileFragment(
             val censurableVisibility =
                 if (hideSensitiveDataIfNecessary && it.id != authenticationContext.userId())
                     GONE else VISIBLE
-            view.findViewById<Button>(R.id.bought_trip_list_shortcut_button).visibility =
-                censurableVisibility
-            view.findViewById<Button>(R.id.trip_list_shortcut_button).visibility =
-                censurableVisibility
-            view.findViewById<TextView>(R.id.nickNameSection).visibility = censurableVisibility
-            view.findViewById<TextView>(R.id.locationSection).visibility = censurableVisibility
+
             nickname.visibility = censurableVisibility
             location.visibility = censurableVisibility
 
--- app/src/main/java/it/polito/mad/group25/lab/fragments/userprofile/ShowUserProfileFragment.kt
+++ app/src/main/java/it/polito/mad/group25/lab/fragments/userprofile/ShowUserProfileFragment.kt
@@ -6,6 +6,8 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import android.widget.RatingBar
+import android.widget.TextView
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import it.polito.mad.group25.lab.R
@@ -16,6 +18,7 @@ class ShowUserProfileFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         visualizeUserData(view, true)
+
         view.findViewById<Button>(R.id.bought_trip_list_shortcut_button).setOnClickListener {
             findNavController()
                 .navigate(R.id.action_showUserProfileFragment_to_BoughtTripsListFragment)
@@ -23,6 +26,56 @@ class ShowUserProfileFragment :
         view.findViewById<Button>(R.id.trip_list_shortcut_button).setOnClickListener {
             findNavController()
                 .navigate(R.id.action_showUserProfileFragment_to_TripListFragment)
+        }
+
+        censure(view)
+        visualizeRating(view)
+    }
+
+    private fun censure(view: View) {
+        userProfileViewModel.shownUser.observe(viewLifecycleOwner) {
+            if (it == null)
+                return@observe
+
+            val censurableVisibility =
+                if (it.id != authenticationContext.userId())
+                    View.GONE else View.VISIBLE
+
+            view.findViewById<Button>(R.id.bought_trip_list_shortcut_button).visibility =
+                censurableVisibility
+            view.findViewById<Button>(R.id.trip_list_shortcut_button).visibility =
+                censurableVisibility
+            view.findViewById<TextView>(R.id.nickNameSection).visibility = censurableVisibility
+            view.findViewById<TextView>(R.id.locationSection).visibility = censurableVisibility
+        }
+    }
+
+    private fun visualizeRating(view: View) {
+        userProfileViewModel.shownUser.observe(viewLifecycleOwner) { it ->
+            if (it == null)
+                return@observe
+
+            val driverRate = reviewViewModel.reviews.values.toList()
+                .filter { reviewVM -> reviewVM.reviewed == it.id && reviewVM.isReviewedDriver == true }
+                .map { reviewVM -> reviewVM.stars }
+            if (driverRate.isNotEmpty()) {
+                var avg = 0F
+                driverRate.forEach { avg += it!! }
+                avg /= driverRate.size
+                view.findViewById<RatingBar>(R.id.driver_rate_bar).rating = avg
+            } else
+                view.findViewById<RatingBar>(R.id.driver_rate_bar).rating = 0F
+
+            val passengerRate = reviewViewModel.reviews.values.toList()
+                .filter { reviewVM -> reviewVM.reviewed == it.id && reviewVM.isReviewedDriver == false }
+                .map { reviewVM -> reviewVM.stars }
+            if (passengerRate.isNotEmpty()) {
+                var avg = 0F
+                passengerRate.forEach { avg += it!! }
+                avg /= passengerRate.size
+                view.findViewById<RatingBar>(R.id.passenger_rate_bar).rating = avg
+            } else
+                view.findViewById<RatingBar>(R.id.passenger_rate_bar).rating = 0F
         }
     }
 
